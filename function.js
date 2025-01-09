af6d9bcfad99899c54dd8a8cade61ae6593a2231
dharnidhar <example@gmail.com>
2025-01-09 13:27:50 +0530

    moving files to git

---
+++ function.js
@@ -0,0 +1,34 @@
+// function demo(){
+//     console.log("function without parameters");
+
+// }
+// demo();
+// function userDetails(name,age,city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// userDetails("sathvika",20,"Warangal");
+// -----function expression------
+// let x= function(){
+//     console.log("Function expression is executing");
+
+// };
+// x();
+//-----IIFE----
+// (function(){
+//     console.log("IIFE");
+// }) ();
+//--arrow function---
+// function demo(){
+//     console.log("Hello");
+
+// }
+// demo();
+// let x=(a,b)=>console.log(a+b);
+// x(5,5);
+
+let x=_=>{console.log("Hi");
+    console.log("hello");
+    console.log("bye");}
+    x();
---
+++ looping.js
@@ -0,0 +1,13 @@
+let n = 100;
+for(let i=0; i<=n;i++)
+{
+    if(i%2 ==0){
+        console.log(i + " is a even number");
+    }else{
+        console.log(i + " is a odd number");
+    }
+}
+let i=1;
+while(i<=5){
+    console.log("Good morning!!!");
+}
